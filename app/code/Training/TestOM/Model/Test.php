5b9036a86462b95c812153ed9ff73cd804bd3ba0
blackstarjaxx <blackstarjaxx07@gmail.com>
2022-06-29 12:23:23 +0300

    Finished task 1.5

--- app/code/Training/TestOM/Model/Test.php
+++ app/code/Training/TestOM/Model/Test.php
@@ -5,17 +5,20 @@ namespace Training\TestOM\Model;
 class Test
 {
     private ManagerInterface $manager;
+    private ManagerInterfaceFactory $managerFactory;
     private array $arrayList;
     private string $name;
     private int $number;
 
     public function __construct(
         \Training\TestOM\Model\ManagerInterface $manager,
+        \Training\TestOM\Model\ManagerInterfaceFactory $managerFactory,
         string $name,
         int $number,
         array $arrayList
     ) {
         $this->manager = $manager;
+        $this->managerFactory = $managerFactory;
         $this->name = $name;
         $this->number = $number;
         $this->arrayList = $arrayList;
@@ -34,6 +37,9 @@ class Test
     public function log()
     {
         print_r(get_class($this->manager));
+        echo '<br>';
+        $newManager = $this->managerFactory->create();
+        print_r(get_class($newManager) . ' task 1.5, instance depends on sequence =_=');
         echo '<br>';
         print_r($this->name);
         echo '<br>';
