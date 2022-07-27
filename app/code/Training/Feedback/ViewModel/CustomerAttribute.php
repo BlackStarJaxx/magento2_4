753327a0267d03d083663c894e23bf7926c3086e
blackstarjaxx <blackstarjaxx07@gmail.com>
2022-07-27 18:07:08 +0300

    Finished 5.3 task

---
+++ app/code/Training/Feedback/ViewModel/CustomerAttribute.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Training\Feedback\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class CustomerAttribute implements ArgumentInterface
+{
+    public function getPersonalSite($customerData)
+    {
+        $attribute = $customerData->getCustomAttribute('personal_site');
+        if ($attribute) {
+            return $attribute->getValue();
+        }
+        return '';
+    }
+
+}
